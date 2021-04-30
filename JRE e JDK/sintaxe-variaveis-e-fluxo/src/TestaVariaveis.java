3ae23b82971c595e03ce744a7c41ae43261efde0
Piimeentaa <dora13.isa@gmail.com>
2021-04-29 23:31:07 -0300

    Porcentagem de aula quinta

---
+++ JRE e JDK/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,19 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Ola novo teste");
+		
+		int idade = 37;
+		System.out.println("Minha idade é: " + idade );
+		
+		idade = 30 + 7;
+		System.out.println("Minha idade é: " + idade);
+		
+		idade = (5 * 6) + 7;
+		System.out.println("Minha idade é: " + idade);
+		
+
+	}
+
+}
